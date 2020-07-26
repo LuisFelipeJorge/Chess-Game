15bcb787e10d0fb08a1b2758c79183e4b00641e2
LuisFelipeJorge <luisfelipe.jorge123@gmail.com>
2020-07-26 10:04:51 -0300

    Creating Chess Postion Class

--- ChessGameProject/Program.cs
+++ ChessGameProject/Program.cs
@@ -8,24 +8,9 @@ namespace ChessGameProject
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Position p = new Position(3, 4);
-                Console.WriteLine(p);
-
-                GameBoard board = new GameBoard(8, 8);
-                board.PutPiece(new King(board, Color.Black), new Position(1, 1));
-                board.PutPiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.PutPiece(new Rook(board, Color.Black), new Position(0, 2));
-
-
-                Screen.PrintGameBoard(board);
-            }
-            catch (GameBoardExceptions e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            
+            ChessPosition pos = new ChessPosition('a', 1);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosition());
         }
     }
 }
---
+++ ChessGameProject/chessGame/ChessPosition.cs
@@ -0,0 +1,29 @@
+﻿
+
+using ChessGameProject.gameBoard;
+
+namespace ChessGameProject.chessGame
+{
+    class ChessPosition
+    {
+        public int Row { get; set; }
+        public char Column { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Position ToPosition()
+        {
+            // Converts the input chess position to a valid index in the matrix
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Row;
+        }
+    }
+}
