f3e8eeff7863eaf119b50b4a29a57a63b90a58ae
LuisFelipeJorge <luisfelipe.jorge123@gmail.com>
2020-07-26 10:17:48 -0300

    Adding colors in the Screen class

--- ChessGameProject/Program.cs
+++ ChessGameProject/Program.cs
@@ -8,9 +8,26 @@ namespace ChessGameProject
     {
         static void Main(string[] args)
         {
-            ChessPosition pos = new ChessPosition('a', 1);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.ToPosition());
+            try
+            {
+                Position p = new Position(3, 4);
+                Console.WriteLine(p);
+
+                GameBoard board = new GameBoard(8, 8);
+                board.PutPiece(new King(board, Color.Black), new Position(1, 1));
+                board.PutPiece(new Rook(board, Color.Black), new Position(0, 0));
+                board.PutPiece(new Rook(board, Color.Black), new Position(0, 2));
+
+                board.PutPiece(new King(board, Color.White), new Position(1, 2));
+                board.PutPiece(new King(board, Color.White), new Position(2, 2));
+
+                Screen.PrintGameBoard(board);
+            }
+            catch (GameBoardExceptions e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
